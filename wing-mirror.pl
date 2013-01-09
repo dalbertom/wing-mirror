c56d006e49ef194ea46aa527f85a1384158bd3f9
David Alberto <david.alberto@appian.com>
2013-01-09 15:13:48 -0500

    store patch id's, files and branches in variables instead of files

--- wing-mirror.pl
+++ wing-mirror.pl
@@ -2,7 +2,6 @@
 
 use strict;
 use warnings;
-goto part2;
 
 my $date = `date -d yesterday +%Y-%m-%d`;
 my @refs = `git for-each-ref --sort="-committerdate" --format="%(committerdate:short) %(refname)" refs/remotes`;
@@ -12,28 +11,23 @@ chomp $date;
 $_ = (split)[1] foreach @refs;
 
 # Get files modified in branches updated today
-die "$!" if -1 == system "rm -f thisids.txt thisfiles.txt thisbranch.txt";
-open(my $fthisids, ">>", "thisids.txt") or die "$!";
-open(my $fthisfiles, ">>", "thisfiles.txt") or die "$!";
-open(my $fthisbranch, ">>", "thisbranch.txt") or die "$!";
-foreach my $thisbranch (@refs) {
-  my @thisfile = `git --no-pager diff --name-only master...$thisbranch -- *.java`;
+my @thisids = ();
+my @thisfiles = ();
+my @thisbranch = ();
+foreach my $ref (@refs) {
+  my @thisfile = `git --no-pager diff --name-only master...$ref -- *.java`;
   chomp @thisfile;
   foreach (@thisfile) {
-    my $thispatchid = `git diff master...$thisbranch -- $_ | git patch-id`;
+    my $thispatchid = `git diff master...$ref -- $_ | git patch-id`;
     chomp $thispatchid;
-    print $fthisids "$thisbranch $thispatchid $_\n";
-    print $fthisfiles "$_\n";
+    push @thisids, "$ref $thispatchid $_";
+    push @thisfiles, $_;
 
   }
-  print $fthisbranch "$thisbranch\n";
+  push @thisbranch, $ref;
 }
-close $fthisids;
-close $fthisfiles;
-close $fthisbranch;
 
 # Get branches unmerged that are not stale
-part2:
 my @thosebranches = `git branch -r --no-merged master`;
 my @nonstale = ();
 foreach(@thosebranches) {
